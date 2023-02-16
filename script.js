b2f6f5f309bd3caf153b0cd6b4d4d85efd9d7185
Lum7n <56406099+Lum7n@users.noreply.github.com>
2023-02-16 18:35:35 +0100

    startettdbjb

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 "use strict";
+var BillardTurnier;
+(function (BillardTurnier) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        console.log("start");
+        buildGroupTables();
+    }
+    function buildGroupTables() {
+        console.log("table");
+        let table = document.createElement("table");
+        let tr1 = document.createElement("tr");
+        let tr2 = document.createElement("tr");
+        let tr3 = document.createElement("tr");
+        let tr4 = document.createElement("tr");
+        let th = document.createElement("th");
+        let td1Name = document.createElement("td");
+        let td1Points = document.createElement("td");
+        let td2Name = document.createElement("td");
+        let td2Points = document.createElement("td");
+        let td3Name = document.createElement("td");
+        let td3Points = document.createElement("td");
+        // <div class="groupA">
+        //     <table>
+        //         <tr>
+        //             <th>Gruppe A</th>
+        //         </tr>
+        //         <tr>
+        //             <td>Max</td>
+        //             <td>0 P.</td>
+        //         </tr>
+        //         <tr>
+        //             <td>Simon</td>
+        //             <td>0 P.</td>
+        //         </tr>
+        //         <tr>
+        //             <td>Justin</td>
+        //             <td>0 P.</td>
+        //         </tr>
+        //     </table>
+        // </div>
+    }
+})(BillardTurnier || (BillardTurnier = {}));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,7 +1,59 @@
 namespace BillardTurnier {
 
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(): void {
+
+        console.log("start");
+
+        buildGroupTables();
 
 
+
+    }
+
+    function buildGroupTables(): void {
+
+        console.log("table");
+
+        let table: HTMLTableElement = document.createElement("table");
+        let tr1: HTMLTableRowElement = document.createElement("tr");
+        let tr2: HTMLTableRowElement = document.createElement("tr");
+        let tr3: HTMLTableRowElement = document.createElement("tr");
+        let tr4: HTMLTableRowElement = document.createElement("tr");
+        let th: HTMLElement = document.createElement("th");
+        let td1Name: HTMLTableCellElement = document.createElement("td");
+        let td1Points: HTMLTableCellElement = document.createElement("td");
+        let td2Name: HTMLTableCellElement = document.createElement("td");
+        let td2Points: HTMLTableCellElement = document.createElement("td");
+        let td3Name: HTMLTableCellElement = document.createElement("td");
+        let td3Points: HTMLTableCellElement = document.createElement("td");
+
+
+
+    // <div class="groupA">
+    //     <table>
+    //         <tr>
+    //             <th>Gruppe A</th>
+    //         </tr>
+    //         <tr>
+    //             <td>Max</td>
+    //             <td>0 P.</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Simon</td>
+    //             <td>0 P.</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Justin</td>
+    //             <td>0 P.</td>
+    //         </tr>
+    //     </table>
+    // </div>
+
+
+
+    }
 
     
 }
